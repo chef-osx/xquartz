848b043458af2d4d91d9068e1b302b305a3c1c60
Julian C. Dunn <jdunn@getchef.com>
2014-09-02 11:43:34 -0400

    Release 1.0.1 with new version of XQuartz

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "opensource@housepub.org"
 license          "Apache 2.0"
 description      "Installs XQuartz on Mac OS X 10.8 to provide an X11 server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends "dmg", ">= 1.1"
 supports "mac_os_x", ">= 10.8"
