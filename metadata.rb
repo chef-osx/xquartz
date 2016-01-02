d9286b2130f45f7b5150e396da532fb89de1b593
Julian C. Dunn <jdunn@aquezada.com>
2016-01-02 13:03:33 -0500

    Update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "xquartz"
 maintainer       "Joshua Timberman"
 maintainer_email "opensource@housepub.org"
 license          "Apache 2.0"
-description      "Installs XQuartz on Mac OS X 10.8 to provide an X11 server."
+description      "Installs XQuartz on Mac OS X to provide an X11 server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.1"
 
